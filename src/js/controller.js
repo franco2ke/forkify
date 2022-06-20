0f6569893b098c68b57504e739df5262ff2bebbb
Francis Wafula <francis@paon.co.ke>
2022-06-20 14:36:23 +0300

    chore(architecture): Refactoring for MVC architecture
    - Add View, Controller, Model and Refactor code
    - Fix display of 'null' and fractional ingredient quantities
    
    [Finishes LEC 292]

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,6 @@
-// In Parcel 2 we can import all kinds of assets including images
-// Use "url: <path>" for static assets that are not programming files; images, videos, sound files etc.
-import icons from 'url:../img/icons.svg';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 // Polyfilling
 import 'core-js/stable'; // Polyfills async await
 import 'regenerator-runtime/runtime'; // Polyfills everything else
@@ -21,166 +21,27 @@ const timeout = function (s) {
 ///////////////////////////////////////
 // LEC 288: Loading a Recipe from API
 
-// Render the Spinner function, while recipe is loading
-const renderSpinner = function (parentEl) {
-  const markup = `
-  <div class="spinner">
-    <svg>
-      <use href="${icons}#icon-loader"></use>
-    </svg>
-  </div>`;
-
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
-
 // load recipe from api and process the response
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     // Obtaining the hash from the window object once hashchange event has been triggered
     // The Window.location read-only property returns a Location object with information about the current location of the document.
     // The Location interface represents the location (URL) of the object it is linked to. Changes done on it are reflected on the object it relates to. Both the Document and Window interface have such a linked Location, accessible via Document.location and Window.location respectively.
     // location.hash - A string containing a '#' followed by the fragment identifier of the URL.
     const id = window.location.hash.slice(1); // slice from position 1 to the end, i.e. remove first character
-    // console.log(id);
+    console.log(id);
     // Guard clause for case where URL has no id
     if (!id) return;
-
-    // 1) loading Recipe
-
     // Render spinner when waiting for image to load
-    renderSpinner(recipeContainer);
+    recipeView.renderSpinner();
 
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-    );
-
-    // Throw custom error
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    const data = await res.json();
-
-    console.log(res, data);
-
-    // Object destructuring
-    let { recipe } = data.data;
-
-    // overwriting the old object to remove underscores
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+    // 1) loading Recipe (async function which returns promise).
+    // An async calling another async where we want to stop execution until a result is returned.
+    // Result will be change of state : state.recipe;
+    await model.loadRecipe(id);
 
     // 2) LEC 289: Rendering the Recipe
-
-    const markup = ` 
-      <figure class="recipe__fig">
-        <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-        <h1 class="recipe__title">
-          <span>${recipe.title}</span>
-        </h1>
-      </figure>
-
-      <div class="recipe__details">
-        <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href="${icons}#icon-clock"></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--minutes">${
-            recipe.cookingTime
-          }</span>
-          <span class="recipe__info-text">minutes</span>
-        </div>
-        <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href="${icons}#icon-users"></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--people">${
-            recipe.servings
-          }</span>
-          <span class="recipe__info-text">servings</span>
-
-          <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-minus-circle"></use>
-              </svg>
-            </button>
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-plus-circle"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
-
-        <div class="recipe__user-generated">
-          <svg>
-            <use href="${icons}#icon-user"></use>
-          </svg>
-        </div>
-        <button class="btn--round">
-          <svg class="">
-            <use href="${icons}#icon-bookmark-fill"></use>
-          </svg>
-        </button>
-      </div>
-
-      <div class="recipe__ingredients">
-        <h2 class="heading--2">Recipe ingredients</h2>
-        <ul class="recipe__ingredient-list">
-          ${recipe.ingredients
-            .map(ing => {
-              return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>`;
-            })
-            // Join() creates and returns a new string by concatenating all the elements in an array, separated by commas or a specified separator string.
-            .join('')}
-        </ul>
-      </div>
-
-      <div class="recipe__directions">
-        <h2 class="heading--2">How to cook it</h2>
-        <p class="recipe__directions-text">
-          This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${
-            recipe.publisher
-          }</span>. Please check out
-          directions at their website.
-        </p>
-        <a
-          class="btn--small recipe__btn"
-          href="${recipe.sourceUrl}"
-          target="_blank"
-        >
-          <span>Directions</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </div>`;
-
-    // Remove place holder html in container
-    recipeContainer.innerHTML = '';
-    // Display updated Recipe
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
   }
@@ -191,15 +52,15 @@ const showRecipe = async function () {
 // NOTE : Listen for the hashchange event
 // the changing of the hash which is a url value, is an event that we can listen for
 // The recipe is loaded everytime a recipe is clicked on.
-// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('hashchange', controlRecipes);
 
 // NOTE: Listen for the page load event
 // Handle's situation where the url is copied to a new browser page in which case there is no change and hence the hashchange event is not triggered.
-// window.addEventListener('load', showRecipe);
+// window.addEventListener('load', controlRecipes);
 
-// Using an array to attach multiple events (hashchange, load) to one callback function (showRecipe)
+// Using an array to attach multiple events (hashchange, load) to one callback function (controlRecipes)
 ['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, showRecipe)
+  window.addEventListener(event, controlRecipes)
 );
 
 ////////////////////////////////////////
@@ -262,3 +123,8 @@ const showRecipe = async function () {
 // Model and View, completely isolated - Don't import anything.
 
 // NOTE: Controller and Model created as Modules, while View created as Class
+
+////////////////////////////////////////
+// LEC 292: Refactoring for MVC
+// 1 file for each different view.
+// Views are simply much bigger
